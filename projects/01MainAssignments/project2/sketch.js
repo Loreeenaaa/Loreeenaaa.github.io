b82e7e82e037840b8c904ed47fc9ca637aa9c30c
Loreeenaaa <151843281+Loreeenaaa@users.noreply.github.com>
2023-11-30 15:58:17 +0100

    Post

--- projects/01MainAssignments/project2/sketch.js
+++ projects/01MainAssignments/project2/sketch.js
@@ -1,168 +1,89 @@
-let circles = [];
-let song;
-let dia = 50;
-let volume = 0;
-let maxDistance;
-let size = 0;
+//font,array,x
 let font;
-let textX, textY;
-let isMouseDragged = false;
-let wellDoneVisible = false;
+let letters = [];
+let x=30
 
+//loading the font
 function preload() {
-	song = loadSound('Steven Beddall - Restless Mind (Instrumental Version).mp3');
-	font= loadFont ('AdobeClean-Bold.otf')
-
+  font = loadFont('AdobeClean-Bold.otf');
 }
 
-
+// text rendering, sets the text size, and then calls function
 function setup() {
-	textFont(font);
-	
-	createCanvas(windowWidth, windowHeight);
-	//maximum distance from the top-left corner to the bottom-right corner of canvas (for scaling)
-	maxDistance = dist(0, 0, width, height)
-	background(0);
-	//text position
-	textX = 30;
-  textY = height - 20;
-	Text();
-	// nested loop for class circle Loop intervals between circles
-	for (let one = dia; one < width - dia; one += dia + dia / 4) {
-		for (let two = dia; two < height - dia; two += dia + dia / 4) {
-			let d = dia;
-			circles.push(new Circle_Loop(one, two, d, color(255)));
-		}
-	}
-	
+  createCanvas(1400, 700);
+  textFont(font);
+  textSize(x);
+  initializeLetters();
 }
 
-
+//white,alpha
 function draw() {
-	noStroke();
-	//loop for circle
-	for (let i = 0; i < circles.length; i++) {
-		//calculate where the circle is
-		let d = dist(mouseX, mouseY, circles[i].x, circles[i].y);
-		size = dia /2.5 + d / maxDistance * dia;
-		gradient = map(size, 0, 60, 255, 0)
-		circles[i].setColor(color(gradient, 0, 0))
-		circles[i].resize(size);
-		circles[i].display();
-		
-	}
-
-}
-
-//interaction_1
-function mouseClicked() {
-  if (wellDoneVisible) {
-    // If "Well done" text is visible, do nothing
-    return;}
-
-	let clicked = false;
-  for (let i = 0; i < circles.length; i++) {
-    let d = dist(mouseX, mouseY, circles[i].x, circles[i].y);
-    size = dia / 2 + d / maxDistance * dia;
-    gradient = map(size, 0, 60, 255, 0);
-
-    if (d < circles[i].diameter / 2) {
-    // Check if the audio is already playing
- 
-				//random volume and displayed number
-        volume = floor(random(101));
-        song.setVolume(volume / 100);
-        circles[i].setColor(color(255, 0, 0));
-        circles[i].setRandomNo(volume);
-        clicked = true;
-			  if (!song.isPlaying()) {
-      		  song.loop();  // Play the audio only if it's not already playing
-				}
-    } else {
-      circles[i].setColor(color(gradient, 0, 0));
-      circles[i].setRandomNo("");
-    }
-    circles[i].resize(size);
-  }
-
-  if (!clicked) {
-    song.pause();
-  }
-}
-
-//interaction_2
-function mouseDragged() {
-  if (!wellDoneVisible) {
-    song.rate(0.6);
-    isMouseDragged = true;
-  }
-}
-
-//release songrate
-function mouseReleased() {
-  if (isMouseDragged) {
-    // Return to normal rate only when the mouse was dragged
-    song.rate(1);
-    isMouseDragged = false;
-  }
-}
-
-//text
-function Text(){
-	fill(0);
-	textSize(20);
-	fill('#A61212');
-	stroke('#610000');
-	strokeWeight(3);
-	text('Adjust the volume to 50% ',textX, textY);
-
-}
-
-class Circle_Loop {
-	constructor(x, y, d, col) {
-		
-  this.x = x;
-  this.y = y;
-  this.diameter = d;
-  this.color = col;
-	this.randomNumber = null; 
-	}
+  background(255,100);
 	
-	//if randomNumber=!null, = number center of the circle
-  display() {
- 	strokeWeight(0.5)
-   fill(this.color);
-	 ellipse(this.x, this.y, this.diameter);
-		
-		 if (this.randomNumber !== null) {
-			stroke(3);
-      fill(0); 
+	//loop for each letter in array; specific position, if hidden
+  for (let i = 0; i < letters.length; i++) {
       textAlign(CENTER, CENTER);
-      textSize(13); 
-      text(this.randomNumber, this.x, this.y);
-			
-			 //if zahl 50 erscheint dann erscheinen
-if (parseInt(this.randomNumber) === 50) {
-	      wellDoneVisible = true; 
-        fill(0);
-        textSize(20);
-       	fill('#A61212');
-				stroke('#610000');
-				strokeWeight(3);
-        text('Well done!', this.x, this.y - 20);
-      }
+      fill(0);
+      text(letters[i].letter, letters[i].x, letters[i].y);
+		//spinning letters
+		if (!letters[i].visible) {
+			 if (frameCount%4 == 0) {
+				 letters[i].letter = getRandomLetter();
+				 
+				 //hiding letters 
+				 if(mouseIsPressed){
+					 letters[i]=frameCount==0
+				 }
+			 }
     }
   }
-	
-	setColor(c) {
-		this.color = c;
-	}
-	setRandomNo(randomNo) {
-		this.randomNumber = randomNo;
-	}
-	
-	resize(d) {
-		this.diameter = d;
-	}
+}
 
+//letters array is initialied by creating objects for each letter at grid positions
+function initializeLetters() {
+	
+  //size of the grid,placement of letters
+	let gridSize = 50;
+ 	for (let x = 0; x < width; x += gridSize) {
+    for (let y = 0; y < height; y += gridSize) {
+      let newLetter = {
+      	x: x + gridSize / 2,
+        y: y + gridSize / 2,
+        letter: getRandomLetter(),
+        visible: true,
+      };
+      letters.push(newLetter);
+    }
+  }
+}
+
+//function to create the random letters
+function getRandomLetter() {
+	
+// Random number 0-25
+let randomIndex = floor(random(26));
+// Convert number to ASCII letters
+let randomLetter = String.fromCharCode(65 + randomIndex);
+return randomLetter;
+}
+
+
+ 
+function mouseMoved() {
+	//circle on cursor
+	noStroke();
+	fill(random(255),random(255),random(255));
+	ellipse(mouseX,mouseY,20);
+	//recognizing where the letters are
+  for (let i = 0; i < letters.length; i++) {
+		if (letters.length != 0) {
+    	let d = dist(mouseX, mouseY, letters[i].x, letters[i].y);
+    	if (d < 15) { //click sensitivity
+			
+			//hide the letters
+      letters[i].visible = false;
+      break; 
+			}
+    }
+  }
 }
